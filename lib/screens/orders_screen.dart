535a1946d897db68e9cdadaded07fd25be64a036
suad-krvavac <suad.krvavac@gmail.com>
2022-07-17 00:30:05 +0200

    Adding an Orders Screen...

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+
+// ignore: use_key_in_widget_constructors
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../providers/orders.dart' as oi;
+
+class OrderItem extends StatelessWidget {
+  final oi.OrderItem order;
+
+  // ignore: prefer_const_constructors_in_immutables, use_key_in_widget_constructors
+  OrderItem(this.order);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
