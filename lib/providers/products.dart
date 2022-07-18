6b2249a5fbd05ed620d3e4b6853538b414602132
suad-krvavac <suad.krvavac@gmail.com>
2022-07-19 00:57:57 +0200

    Updating Data via PATCH Requests...

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -140,9 +140,21 @@ class Products with ChangeNotifier {
     // });
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.https(
+        'shop-flutter-max-default-rtdb.europe-west1.firebasedatabase.app',
+        'products/$id.json',
+      );
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+            // 'isFavorite': newProduct.isFavorite,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
