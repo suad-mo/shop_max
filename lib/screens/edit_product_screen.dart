7fa0080cdc33aa8ae3bfe1e6794b76bfdc7c0e75
suad-krvavac <suad.krvavac@gmail.com>
2022-07-18 12:45:36 +0200

    Saving New Products...

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../providers/product.dart';
+import '../providers/products.dart';
 
 // ignore: use_key_in_widget_constructors
 class EditProductScreen extends StatefulWidget {
@@ -58,11 +60,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
       return;
     }
     _form.currentState?.save();
-    // print(_editedProduct.title);
-    // print(_editedProduct.description);
-    // print(_editedProduct.price);
-    // print(_editedProduct.imageUrl);
-    // print(_editedProduct.id);
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
   }
 
   @override
