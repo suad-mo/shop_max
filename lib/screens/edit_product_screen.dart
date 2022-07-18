654283bef5f5a102269c20128df6dab32301cc38
suad-krvavac <suad.krvavac@gmail.com>
2022-07-18 11:33:58 +0200

    Validating User Input...

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -46,12 +46,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState?.validate();
+    if (!isValid!) {
+      return;
+    }
     _form.currentState?.save();
-    print(_editedProduct.title);
-    print(_editedProduct.description);
-    print(_editedProduct.price);
-    print(_editedProduct.imageUrl);
-    print(_editedProduct.id);
+    // print(_editedProduct.title);
+    // print(_editedProduct.description);
+    // print(_editedProduct.price);
+    // print(_editedProduct.imageUrl);
+    // print(_editedProduct.id);
   }
 
   @override
@@ -77,6 +81,14 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   textInputAction: TextInputAction.next,
                   onFieldSubmitted: (_) {
                     FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                  validator: (value) {
+                    // return null;//ovo znači nema greške
+                    // return 'This is wrong';//ovo je text greške
+                    if (value!.isEmpty) {
+                      return 'Please provide a value.';
+                    }
+                    return null;
                   },
                   onSaved: (value) {
                     _editedProduct = Product(
